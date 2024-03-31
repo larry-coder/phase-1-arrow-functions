e38d43e4ede582f46c8b0d8558535d8b1cf9932d
omorut <hillarydavisomorut@gmail.com>
2024-03-31 04:06:32 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (a ,b) => {
+    return a/b;
+}
+console.log(divide(2000, 100));
+
+const square = (x) => {
+    return x**2;
+}
+console.log(square(2));
+
+const add = (a, b) => {
+    return a + b;
+}
+console.log(add(5, 2));
